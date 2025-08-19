71dcf881910f7d557055a3e38b20451539c78aa5
frank-in-code <frankpintomuniz30@gmail.com>
2025-08-19 20:00:35 -0300

    Coloquei alguns comentários para explicar um pouco mais sobre os componente, inclui o std::endl para quebrar uma linha também

--- 01-fundamentos/helloworld.cpp
+++ 01-fundamentos/helloworld.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <iostream> 
+// incluir a biblioteca iostream (entrada e saída de dados)
+
+// Comentários ajudam a organizar e explicar o código (//)
+// Eles não são executados pelo programa
 
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+    // main é a função principal do programa
+    // int indica que ela retorna um valor inteiro (0 = sucesso)
+
+    // std::cout envia dados para a saída padrão (console)
+    // << é o operador de inserção, usado para enviar dados
+    std::cout << "Hello World!" << std::endl; // imprime a mensagem na tela
+
+    return 0; // retorna 0 para o sistema operacional, indicando que terminou corretamente
 }
